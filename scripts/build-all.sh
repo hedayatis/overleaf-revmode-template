0b59e088a6a23d496001ef9ddb45e2ab5b907146
hedayatis <63190657+hedayatis@users.noreply.github.com>
2026-09-02 15:04:39 +0000

    Add separate response-letter workflow for v1.1.0

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -5,3 +5,4 @@ cd "$repo_dir"
 latexmk -pdf -interaction=nonstopmode -halt-on-error main-original.tex
 latexmk -pdf -interaction=nonstopmode -halt-on-error main-comments.tex
 latexmk -pdf -interaction=nonstopmode -halt-on-error main-final.tex
+latexmk -pdf -interaction=nonstopmode -halt-on-error main-response.tex
